35ef6b18f5d97744fa16559d3faabb71c29c510c
Nguyễn Quang Minh (NQM) <nguyenquangminh391@gmail.com>
2025-04-12 16:25:30 +0700

    Initial Commit

---
+++ quickmem/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  quickmem
+//
+//  Created by Nguyen Quang Minh on 4/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ quickmem/quickmemApp.swift
@@ -0,0 +1,17 @@
+//
+//  quickmemApp.swift
+//  quickmem
+//
+//  Created by Nguyen Quang Minh on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct quickmemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
