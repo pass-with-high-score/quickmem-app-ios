c836a22c1aaa57417423581a8e0f94cc839df9c9
VawnDao <134820492+VawnDao@users.noreply.github.com>
2025-04-20 17:16:07 +0700

    feat: signup ui (#1)
    * feat: finish ui signup view
    
    * feat: add some missing Vietnamese translations

--- quickmem/Presentation/Auth/Signup/View/SignupView.swift
+++ quickmem/Presentation/Auth/Signup/View/SignupView.swift
@@ -8,8 +8,115 @@
 import SwiftUI
 
 struct SignupView: View {
+    @State private var isSignupWithEmail = false
     var body: some View {
-        Text("Signup")
+        NavigationStack {
+            ZStack {
+                LinearGradient(
+                    colors: [
+                        Color.primaryColor.opacity(0.3),
+                        Color.white,
+                        Color.white,
+                        Color.white,
+                        Color.white,
+                    ],
+                    startPoint: .topTrailing,
+                    endPoint: .bottomTrailing
+                ).ignoresSafeArea()
+                VStack(spacing: 16) {
+                    Spacer()
+                        .frame(height: 32)
+                    Text("signup_subtitle")
+                        .font(.body)
+                    
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 32)
+                        .padding(.bottom, 150)
+                    
+                    AuthButton(
+                        icon: Image(systemName: "envelope"),
+                        title: String(localized: "signup_with_email"),
+                        action: {
+                            withAnimation{
+                                isSignupWithEmail.toggle()
+                            }
+                        },
+                        backgroundColor: .blue
+                    )
+                    
+                    HStack {
+                        Divider()
+                            .frame(width: 100, height: 1)
+                            .background(Color.gray.opacity(0.5))
+                        
+                        Text("login_or")
+                            .foregroundColor(.primaryColor)
+                            .padding(.horizontal, 32)
+                            .padding(.vertical, 8)
+                        
+                        Divider()
+                            .frame(width: 100, height: 1)
+                            .background(Color.gray.opacity(0.5))
+                    }
+                    
+                    AuthButton(
+                        icon: Image(AppIcons.icGoogle),
+                        title: String(localized: "login_with_google"),
+                        action: {
+                            // google login logic
+                        },
+                        backgroundColor: .white,
+                        foregroundColor: .black
+                    )
+                    
+                    AuthButton(
+                        icon: Image(systemName: "apple.logo"),
+                        title: String(localized: "login_with_apple"),
+                        action: {
+                            // apple login logic
+                        },
+                        backgroundColor: .black
+                    )
+                    (
+                        Text("signup_by_signing_up_you_agree_to_the")
+                        + Text("signup_terms_and_conditions")
+                            .underline()
+                            .foregroundColor(.blue)
+                            .fontWeight(.bold)
+                        + Text("signup_and_the")
+                        + Text("signup_privacy_policy")
+                            .fontWeight(.bold)
+                            .underline()
+                            .foregroundColor(.blue)
+                        + Text("signup_of_quickmem")
+                    )
+                    .multilineTextAlignment(.center)
+                    .font(.body)
+                    
+                    NavigationLink(destination: SignupView()) {
+                        HStack {
+                            Text("signup_already_have_an_account")
+                            Text("signup_login_button")
+                                .underline()
+                                .font(.body)
+                                .fontWeight(.bold)
+                                .foregroundColor(.blue)
+                        }
+                    }
+                    .buttonStyle(.plain)
+                    
+                    Spacer()
+                }
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        Text("signup_title")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(.primaryColor)
+                    }
+                }
+            }
+        }
     }
 }
 
