2f26d95d42777b026ed9fa04fbdadbdc9a506ae8
VawnDao <hadao1204@gmail.com>
2025-04-20 10:07:50 +0700

    feat: finish ui signup view

---
+++ quickmem/Presentation/Auth/Signup/SignupWithEmail/View/SignupWithEmailView.swift
@@ -0,0 +1,17 @@
+//
+//  SignupWithEmailView.swift
+//  quickmem
+//
+//  Created by Hà Đạo on 20/4/25.
+//
+import SwiftUI
+
+struct SignupWithEmailView: View {
+    var body: some View {
+        Text("SignupWithEmailView")
+    }
+}
+
+#Preview {
+    SignupWithEmailView()
+}
---
+++ quickmem/Presentation/Auth/Signup/SignupWithEmail/ViewModel/SignupWithEmailViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  SignupWithEmailViewModel.swift
+//  quickmem
+//
+//  Created by Hà Đạo on 20/4/25.
+//
+import Foundation
+
+class SignupWithEmailViewModel: ObservableObject {
+    
+}
